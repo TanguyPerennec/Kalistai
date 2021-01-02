0e75a53813d3dc94e8ab4456ceec9ed0d04bad4e
Tangzer <perennec.tanguy@gmail.com>
2021-01-02 18:42:56 +0100

    Correction setup

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import find_packages, setup
 
 setup(
-    name=’Kalistai’,
-    packages=find_packages(include=[‘mypythonlib’]),
-    version=’0.0.1',
+    name='Kalistai',
+    packages=find_packages(include=['mypythonlib']),
+    version='0.0.1',
     url='https://github.com/TanguyPerennec/Kalistai',
-    description=’Basic function to facilitate DL’,
-    author=’Kalistai group’,
-    license=’MIT’
+    description='Basic function to facilitate DL',
+    author='Kalistai group',
+    license='MIT'
 )
+
