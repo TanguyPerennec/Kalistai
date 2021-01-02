24a08326ce2a95eeeddc9a79c040a8d6800aa9f3
Tangzer <perennec.tanguy@gmail.com>
2021-01-02 18:41:03 +0100

    Setup initial

--- setup.py
+++ setup.py
@@ -4,11 +4,8 @@ setup(
     name=’Kalistai’,
     packages=find_packages(include=[‘mypythonlib’]),
     version=’0.0.1',
+    url='https://github.com/TanguyPerennec/Kalistai',
     description=’Basic function to facilitate DL’,
     author=’Kalistai group’,
-    license=’MIT’,
-    install_requires=[],
-    setup_requires=[‘pytest-runner’],
-    tests_require=[‘pytest==4.4.1’],
-    test_suite=’tests’,
+    license=’MIT’
 )
